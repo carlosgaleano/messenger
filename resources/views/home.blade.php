25e855141d3226bdcb8ebe7971ec66e194726b7e
carlosgaleano <carlos.galeano@cellstarch.com>
2020-06-12 17:04:02 -0400

    de

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<b-container fluid>
+    <b-row>
+        <b-col cols="4">
+            <b-form-input class="text-center"
+            
+            type="text"
+            required
+            placeholder="Buscar contacto ..."
+            ></b-form-input> 
+            <b-row>
+                <b-col cols="3">
+                    <b-img v-bind="mainProps"  blank="true"  blankColor='#777'  width =75 height='75'  rounded="circle" alt="img"></b-img>
+                </b-col>
+                <b-col cols="7"></b-col>
+                <b-col cols="2"></b-col>
+            </b-row>
+        </b-col>
+        <b-col cols="8"></b-col>
+    </b-row>
+</b-container>
 @endsection
