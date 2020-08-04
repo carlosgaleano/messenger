ae67473d5e8b28f1f89a5afe633896b271d804c2
carlosgaleano <carlos.galeano.solano@gmail.com>
2020-08-04 17:17:03 -0400

    saving04082020

--- resources/js/components/ContactComponent.vue
+++ resources/js/components/ContactComponent.vue
@@ -9,7 +9,7 @@
                 <p class="text-muted small mb-1">{{ conversation.last_message }}</p>
             </b-col>
             <b-col cols="3"  class="d-none d-md-block" >
-            <p class="text-muted small">{{ conversation.last_time }}</p>
+            <p class="text-muted small">{{ lastTime }}</p>
             </b-col>
         </b-row>
     </b-list-group-item>
@@ -27,5 +27,12 @@ export default {
 
         };
     },
+    computed:{
+        lastTime(){
+            return moment(this.conversation.last_time,"YYYY-MM-DD hh:mm:ss a")
+            .locale('es').fromNow();
+
+        }
+    }
 }
 </script>
